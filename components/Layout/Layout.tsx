dacf7988d38dc2c4f16ae1465d15ccf1d46168fc
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-11-10 21:21:32 -0500

    Change secondary color to turqoise

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,17 @@
+import { TypographyStyle, GoogleFont } from "react-typography";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.scss";
+import Head from "next/head";
+import typography from "../../styles/typography";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
+      {/* <head>
+        <TypographyStyle typography={typography} />
+        <GoogleFont typography={typography} />
+      </head> */}
       <Navbar />
       <div className={styles.container}>
         {children}
